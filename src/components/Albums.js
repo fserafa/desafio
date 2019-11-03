ab262820c0148e08950c41e606dde665e54cda6f
Felipe <felipe.seraf@gmail.com>
2019-11-03 13:34:51 -0300

    more styles

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import { Card, ListGroup } from 'react-bootstrap'
+import { Card } from 'react-bootstrap';
+import Arrow from '../assets/arrow.png';
+import './Albums.css';
 
 const Albums = (props) => {
     return (
         props.data.map(album => (
-            <a href="" key={album.collectionId} onClick={() => props.handleAlbumClick(album.artistId, album.collectionId)}>
-                <Card className="border-right-0 border-left-0 border-bottom-0 rounded-0" >
-                        <Card.Body>
-                            <div className="d-flex justify-content-start ">
-                                <div>
-                                    <img src={album.artworkUrl100} alt={album.collectionName} />
-                                </div>
-                                <div className="ml-2 d-flex flex-column justify-content-center">
-                                    <p className="font-weight-bold mb-0">{album.collectionName}</p>
-                                    <p className="mb-0">{album.artistName}</p>
-                                </div>
-                                <div className="ml-2 d-flex flex-column justify-content-center">
-                                    <p className="font-weight-bold mb-0">></p>
-                        
-                                </div>
+            <Card
+                className="border-right-0 border-left-0 border-bottom-0 rounded-0 card-list"
+                key={album.collectionId}
+                onClick={() => props.handleAlbumClick(album.artistId, album.collectionId)}>
+                <Card.Body>
+                    <div className="d-flex justify-content-between ">
+                        <div className="ml-2 d-flex flex-row justify-content-center">
+                            <div>
+                                <img src={album.artworkUrl100} alt={album.collectionName} />
                             </div>
-                        </Card.Body>
-                </Card>
-            </a>
+
+                            <div className="ml-2 d-flex flex-column justify-content-center">
+                                <p className="font-weight-bold mb-0">{album.collectionName}</p>
+                                <p className="mb-0">{album.artistName}</p>
+                            </div>
+                        </div>
+                        <div className="d-flex flex-column justify-content-center">
+                            <img src={Arrow} alt="Arrow-right" />
+
+                        </div>
+                    </div>
+
+                </Card.Body>
+            </Card>
+
         ))
     );
 };
